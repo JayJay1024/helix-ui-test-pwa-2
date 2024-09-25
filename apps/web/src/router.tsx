501e71189b7632c60c29de1b9d5ef20e29677dbc
JayJay1024 <lygm1062@gmail.com>
2024-09-25 23:10:14 +0800

    18

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -4,18 +4,18 @@ import Home from "./routes/home";
 import Relayer from "./routes/relayer";
 import NotFound from "./routes/not-found";
 import Error from "./routes/error";
-// import Explorer from "./routes/explorer";
+import Explorer from "./routes/explorer";
 // import TxDetails from "./routes/tx-details";
-import { Debug } from "./components/debug";
+// import { Debug } from "./components/debug";
 
 export const router = createHashRouter([
   {
     element: <Root />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/a", element: <Debug /> },
+      // { path: "/a", element: <Debug /> },
       { path: "/relayer", element: <Relayer /> },
-      // { path: "/explorer", element: <Explorer /> },
+      { path: "/explorer", element: <Explorer /> },
       // { path: "/tx/:id", element: <TxDetails /> },
       { path: "*", element: <NotFound /> },
     ],
