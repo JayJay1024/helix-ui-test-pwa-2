78b7c31c3d09cbca457160dfb7a25d9bafccf728
JayJay1024 <lygm1062@gmail.com>
2024-09-25 22:41:35 +0800

    12

--- apps/web/src/components/footer.tsx
+++ apps/web/src/components/footer.tsx
@@ -27,7 +27,7 @@ function Links() {
   const data: TData[] = [
     { label: "Explorer", path: "/explorer" },
     { label: "XToken", path: "https://xtoken.box", external: true },
-    { label: "Docs11", path: "https://docs.helixbridge.app", external: true },
+    { label: "Docs12", path: "https://docs.helixbridge.app", external: true },
   ];
   const [network, setNetwork] = useState<TData>({ label: "Saa", path: "ss", pc: true, external: true });
 
--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -1,10 +1,10 @@
 import { createHashRouter } from "react-router-dom";
 import Root from "./routes/root";
-import Home from "./routes/home";
+// import Home from "./routes/home";
 import Relayer from "./routes/relayer";
 import NotFound from "./routes/not-found";
 import Error from "./routes/error";
-// import Explorer from "./routes/explorer";
+import Explorer from "./routes/explorer";
 import TxDetails from "./routes/tx-details";
 // import { Debug } from "./components/debug";
 
@@ -12,10 +12,10 @@ export const router = createHashRouter([
   {
     element: <Root />,
     children: [
-      { path: "/", element: <Home /> },
+      // { path: "/", element: <Home /> },
       // { path: "/", element: <Debug /> },
       { path: "/relayer", element: <Relayer /> },
-      // { path: "/explorer", element: <Explorer /> },
+      { path: "/explorer", element: <Explorer /> },
       { path: "/tx/:id", element: <TxDetails /> },
       { path: "*", element: <NotFound /> },
     ],
