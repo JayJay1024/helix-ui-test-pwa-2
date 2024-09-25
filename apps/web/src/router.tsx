f265d00b982f1cb704db0bd217d5116b0821403d
JayJay1024 <lygm1062@gmail.com>
2024-09-25 22:28:04 +0800

    test

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -1,11 +1,11 @@
 import { createHashRouter } from "react-router-dom";
 import Root from "./routes/root";
 import Home from "./routes/home";
-import Relayer from "./routes/relayer";
-import NotFound from "./routes/not-found";
+// import Relayer from "./routes/relayer";
+// import NotFound from "./routes/not-found";
 import Error from "./routes/error";
-import Explorer from "./routes/explorer";
-import TxDetails from "./routes/tx-details";
+// import Explorer from "./routes/explorer";
+// import TxDetails from "./routes/tx-details";
 // import { Debug } from "./components/debug";
 
 export const router = createHashRouter([
@@ -14,10 +14,10 @@ export const router = createHashRouter([
     children: [
       { path: "/", element: <Home /> },
       // { path: "/", element: <Debug /> },
-      { path: "/relayer", element: <Relayer /> },
-      { path: "/explorer", element: <Explorer /> },
-      { path: "/tx/:id", element: <TxDetails /> },
-      { path: "*", element: <NotFound /> },
+      // { path: "/relayer", element: <Relayer /> },
+      // { path: "/explorer", element: <Explorer /> },
+      // { path: "/tx/:id", element: <TxDetails /> },
+      // { path: "*", element: <NotFound /> },
     ],
     errorElement: <Error />,
   },
